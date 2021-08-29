2ed1a0b210ba4d68938a736bf5fb6cd0cdb71995
siddvarman <89657348+siddvarman@users.noreply.github.com>
2021-08-29 22:02:45 +0530

    Update Activity_12.py

---
+++ Activity_12.py
@@ -0,0 +1,14 @@
+#First we have to get three numbers from user
+num1 = int(input("Enter number 1:"))
+num2 = int(input("Enter number 2:"))
+num3 = int(input("Enter number 3:"))
+#Using 'if' condition to see the greatest number
+if(num1>num2) and (num1>num3):
+    print(num1,"Is the greatest among two numbers")
+elif(num2>num1) and (num2>num3):
+        print(num2,"Is the greatest among two numbers")
+elif(num3>num1) and (num3>num2):
+            print(num3,"Is the greatest among two numbers")
+            #Else used if all three are same
+else:
+                print("Three numbers are equal")
